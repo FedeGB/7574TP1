00fbeca1450c44b10aef308550a8c6a2f7cb8242
FedeGB <fedeagb@gmail.com>
2017-06-04 15:27:12 -0300

    Eliminadas constantes que ya no se usan mas en el cliente.

--- Cliente/Constantes.h
+++ Cliente/Constantes.h
@@ -21,33 +21,12 @@
 #define QREGISTROCLOUTPATH "/bin/echo"
 #define QREGISTROCLOUTID 6
 
-// Semaforos
-#define SEMLUGARESPATH "/bin/sh"
-#define SEMLUGARESID 2
-#define SEMLUGARESCAJPATH "/bin/sh"
-#define SEMLUGARESCAJID 3
-#define SEMENTRADAPATH "/bin/sh"
-#define SEMENTRADAID 4
-
-// Shared Memory
-#define ENTRADAPATH "/bin/dir"
-#define ENTRADAID 1
-#define LUGARESPATH "/bin/dir"
-#define LUGARESID 2
-#define LUGARESCAJEROPATH "/bin/dir"
-#define LUGARESCAJEROID 3
 
 // TIPO ÞEDIDO
 #define LLEVAR '1'
 #define ENLUGAR '2'
 
-// Variables heladeria
-#define ESPACIOHELADERIA 10
-#define MAXCOLACAJER 4
-
 // Socket
-//#define CLIENTEIP "127.0.0.1"
-//#define CLIENTEPORT 8081
 #define BROKERIP "127.0.0.1"
 #define BROKERPORT 8085
 
